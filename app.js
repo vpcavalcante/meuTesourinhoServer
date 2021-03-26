08b64337393e58671e5ac112f2193621e057a893
ligiadavilabozzi <ligia.bozzi@gmail.com>
2021-03-26 19:50:46 -0300

    atualização da dashboard

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+//var usersRouter = require('./routes/users');
 var dashboardRouter = require('./routes/dashboard');
 var faqRouter = require('./routes/faq');
 var contatoRouter = require('./routes/contato')
@@ -23,10 +23,11 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//app.use('/users', usersRouter);
 app.use('/dashboard', dashboardRouter);
 app.use('/faq', faqRouter);
 app.use('/contato', contatoRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
