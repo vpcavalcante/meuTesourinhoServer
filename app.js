4b5f1fb66b0b1964d65a0d5448bff949e5e0568e
ligiadavilabozzi <ligia.bozzi@gmail.com>
2021-03-26 20:56:06 -0300

    atualização da trilha e do header

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-//var usersRouter = require('./routes/users');
+var usersRouter = require('./routes/users');
 var dashboardRouter = require('./routes/dashboard');
 var faqRouter = require('./routes/faq');
 var contatoRouter = require('./routes/contato')
