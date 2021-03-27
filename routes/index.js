971ee2e3642d8180b9c4806a6d7401782c5d113c
Jessika <jessika.msmacedo@gmail.com>
2021-03-26 21:09:07 -0300

    incluindo header, head, footer no index

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/index')
-/* GET dashboard. */
+/* GET index. */
 router.get('/', controller.index);
 
 module.exports = router;
